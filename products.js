450f172254ae3c01b496b8b976c906856ffd358a
pipahcookiesapp <mbi5385@gmail.com>
2025-11-12 15:09:27 +0800

    Update products.js

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 // --- PRODUCT DATA: Update this file daily for stock/price changes ---
 const products = [
     // Format: { id: [ID], name: '[Product Name]', price: [0.00], isVideo: [true/false], stock: [Qty], mediaUrl: 'Images/filename.jpg' },
-    { id: 1, name: 'Osaka Creampuff 20pcs', price: 10.00, isVideo: false, stock: 10, mediaUrl: 'Images/item1.jpg' }, 
+    { id: 1, name: 'Japanese Creampuff 15pcs', price: 10.00, isVideo: false, stock: 10, mediaUrl: 'Images/jc1.jpg' }, 
     { id: 2, name: 'Roti Golok', price: 5.50, isVideo: false, stock: 10, mediaUrl: 'Images/item2.jpg' }, 
     { id: 3, name: 'Seasalt Cookies 65pcs', price: 16.00, isVideo: false, stock: 20, mediaUrl: 'Images/ss1.jpg' },
     
