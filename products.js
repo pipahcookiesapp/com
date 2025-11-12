80bf6043091a1898b1039a26a31c309c88f0bab6
pipahcookiesapp <mbi5385@gmail.com>
2025-11-12 13:51:11 +0800

    Update products.js

--- products.js
+++ products.js
@@ -4,6 +4,10 @@ const products = [
     { id: 1, name: 'Osaka Creampuff 20pcs', price: 10.00, isVideo: false, stock: 10, mediaUrl: 'Images/item1.jpg' }, 
     { id: 2, name: 'Roti Golok', price: 5.50, isVideo: false, stock: 10, mediaUrl: 'Images/item2.jpg' }, 
     { id: 3, name: 'Seasalt Cookies 65pcs', price: 16.00, isVideo: false, stock: 20, mediaUrl: 'Images/ss1.jpg' },
-    // New Product Example (You can now easily add the URL here)
-    ];
+    
+    // --- NEW PLACEHOLDER SLOTS (Set to 0 stock) ---
+    { id: 4, name: 'New Product Slot 4', price: 0.00, isVideo: false, stock: 0, mediaUrl: 'Images/item4.jpg' },
+    { id: 5, name: 'New Product Slot 5', price: 0.00, isVideo: false, stock: 0, mediaUrl: 'Images/item5.jpg' },
+    { id: 6, name: 'New Product Slot 6', price: 0.00, isVideo: false, stock: 0, mediaUrl: 'Images/item6.jpg' }
+];
 // --- END OF PRODUCT DATA ---
