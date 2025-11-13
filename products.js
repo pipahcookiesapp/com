a56bb3f07df583f6d535431d9a2e0b5e65bed9a6
pipahcookiesapp <mbi5385@gmail.com>
2025-11-13 23:35:15 +0800

    Update products.js

--- products.js
+++ products.js
@@ -1,13 +1,13 @@
 // --- PRODUCT DATA: Update this file daily for stock/price changes ---
 const products = [
     // Format: { id: [ID], name: '[Product Name]', price: [0.00], isVideo: [true/false], stock: [Qty], mediaUrl: 'Images/filename.jpg' },
-    { id: 1, name: 'Japanese Creampuff 15pcs', price: 10.00, isVideo: false, stock: 10, mediaUrl: 'Images/jc1.jpg' }, 
+    { id: 1, name: 'Japanese Crmpuff 15pcs', price: 10.00, isVideo: false, stock: 10, mediaUrl: 'Images/jc1.jpg' }, 
     { id: 2, name: 'Roti Golok', price: 5.50, isVideo: false, stock: 10, mediaUrl: 'Images/item2.jpg' }, 
     { id: 3, name: 'Seasalt Cookies 65pcs', price: 16.00, isVideo: false, stock: 20, mediaUrl: 'Images/ss1.jpg' },
     
     // --- NEW PLACEHOLDER SLOTS (Set to 0 stock) ---
-    { id: 4, name: 'New Product Slot 4', price: 0.00, isVideo: false, stock: 0, mediaUrl: 'Images/item4.jpg' },
-    { id: 5, name: 'New Product Slot 5', price: 0.00, isVideo: false, stock: 0, mediaUrl: 'Images/item5.jpg' },
+    { id: 4, name: 'Osaka Crmpuff 20pcs', price: 10.00, isVideo: false, stock: 10, mediaUrl: 'Images/oc1.jpg' },
+    { id: 5, name: 'Black Cookies(80pcs)', price: 19.00, isVideo: false, stock: 20, mediaUrl: 'Images/bc5.jpg' },
     { id: 6, name: 'Kek Choc Moist 4`', price: 10.00, isVideo: false, stock: 10, mediaUrl: 'Images/cm1.jpg' }
 ];
 // --- END OF PRODUCT DATA ---
